9d6e9826ebfcfe1962dd6e6501f742025b9b22b4
Adrie <a.kooijman@gmail.com>
2023-08-28 18:04:35 +0200

    Update WebLETTChart.js
    cleanup formatting

--- docs/WebLETTChart.js
+++ docs/WebLETTChart.js
@@ -1,67 +1,45 @@
-		var myChart = new Chart(document.getElementById("myChart"), {
-		type: 'scatter',
-		data: {
-			datasets: [
-			{
-				label: "Force",
-				showLine: true,
-				borderColor: '#d45087', // #FF6384',
-				fill: false,
-				data: []
-			},
-			{
-				label: "Resistance",
-				showLine: true,
-				hidden: true,
-				borderColor: '#665191', //'#36A2EB',
-				fill: false,
-				data: []
-			},
-			{
-				label: "Temperature",
-				showLine: true,
-				hidden: true,
-				borderColor: '#003f5c', //'lightBlue',
-				fill: false,
-				data: [{x:7, y:6}, {x:15, y:16}]
+var myChart = new Chart(document.getElementById("myChart"), {
+	type: 'scatter',
+	data: {
+		datasets: [{
+			label: "Force",
+			showLine: true,
+			borderColor: '#d45087', // #FF6384',
+			fill: false,
+			data: []
+		}, {
+			label: "Resistance",
+			showLine: true,
+			hidden: true,
+			borderColor: '#665191', //'#36A2EB',
+			fill: false,
+			data: []
+		}, {
+			label: "Temperature",
+			showLine: true,
+			hidden: true,
+			borderColor: '#003f5c', //'lightBlue',
+			fill: false,
+			data: [{x:7, y:6}, {x:15, y:16}]
+		}]
+	},
+	options: {
+		responsive: true,
+		scales: {
+			xAxes: [{
+			//type: 'linear', 
+			scaleLabel: {
+				display: true,
+				labelString: 'extension',
 			}
-			]
-		},
-		options: {
-		  responsive: true,
-
-/*
-		  scales: {
-			xAxes: [{
-				scaleLabel: {
-					display: true,
-					labelString: 'extension'
-				}
 			}],
 			yAxes: [{
-				scaleLabel: {
-					display: true,
-					labelString: 'force'
-				}
+			//type: 'linear', 
+			scaleLabel: {
+				display: true,
+				labelString: 'force'
+			}
 			}]
-		  }
-*/
-		  scales: {
-			xAxes: [{
-				//type: 'linear', 
-				scaleLabel: {
-					display: true,
-					labelString: 'extension',
-				}
-			}],
-			yAxes: [{
-				//type: 'linear', 
-				scaleLabel: {
-					display: true,
-					labelString: 'force'
-				}
-			}]
-		  }
-
-	      }
-		});
+		}
+	}
+});
