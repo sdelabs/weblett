bb4c38e41595a3a3e17be049cd37b1d18e506019
Adrie <a.kooijman@gmail.com>
2023-09-04 15:08:14 +0200

    Update bt.js

--- docs/bt.js
+++ docs/bt.js
@@ -65,20 +65,17 @@ console.log('Getting Service...', serviceName);
 //***
 
 	incomingCharacteristic = await service.getCharacteristic(characteristicName);
-	incomingCharacteristic.addEventListener('characteristicvaluechanged', handleIncoming);
+	incomingCharacteristic.addEventListener('characteristicvaluechanged', getData);
 	await incomingCharacteristic.startNotifications(); // otherwise nothing happens...
 	timeStarted = Date.now();
 
-//TODO: we need a writebackservice for HM10 (LETT, too)
-	if (server.device.name.startsWith("CIRCUITPY")) {
-		console.log('Getting outgoing Characteristic...');
-		outgoingCharacteristic = await service.getCharacteristic('6e400002-b5a3-f393-e0a9-e50e24dcca9e');
-		message = enc.encode("test");
-		if (debug) {
-			console.log('write test message');
-		}
-		outgoingCharacteristic.writeValue(message);
+	console.log('Getting outgoing Characteristic...');
+	outgoingCharacteristic = await service.getCharacteristic('0000ffe1-0000-1000-8000-00805f9b34fb');
+	message = enc.encode("test");
+	if (debug) {
+		console.log('write test message');
 	}
+	outgoingCharacteristic.writeValue(message);
 }
 
 async function onDisconnected() {
@@ -88,4 +85,9 @@ async function onDisconnected() {
 	} catch(error) {
 		console.log('*** Argh! (onDisconnect) ' + error);
 	}
+}
+
+function getData(event) {
+	console.log('getData', event)
+	console.log('getData target value', event.target.value)
 }
