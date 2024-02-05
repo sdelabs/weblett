e10b4d9f98f10f221fc1c3293ea7953abe9b9c90
SDE Labs account <152285936+sdelabs@users.noreply.github.com>
2024-02-05 17:09:07 +0100

    Update WebLETT.js

--- docs/WebLETT.js
+++ docs/WebLETT.js
@@ -531,9 +531,9 @@ function processIncoming(line) {
 
 function plotData(p) {
 	if (debug) console.log('plotData', p);
-	myChart.config.data.datasets[0].data.push({x: p[posTim], y:p[posFor]}) // F
-	myChart.config.data.datasets[1].data.push({x: p[posTim], y:p[posPos]}) // R
-	myChart.config.data.datasets[2].data.push({x: p[posPos], y:p[posFor]}) // K
+	myChart.config.data.datasets[0].data.push({x: p[posPos], y:p[posFor]}) // Displacement vs Force
+	myChart.config.data.datasets[1].data.push({x: p[posTim], y:p[posFor]}) // Time vs Force
+	myChart.config.data.datasets[2].data.push({x: p[posTim], y:p[posPos]}) // Time vs Resistance
 	myChart.update()	
 }
 
