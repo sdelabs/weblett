93a6a789c4dea6cde813868b05c940c899bba80a
SDE Labs account <152285936+sdelabs@users.noreply.github.com>
2024-07-31 11:07:52 +0200

    Update WebLETT.js

--- docs/WebLETT.js
+++ docs/WebLETT.js
@@ -491,11 +491,12 @@ function processIncoming(line) {
 		if (data[i].startsWith('r')) { // resistance
 			//console.log('f', data[i].split('f'));
 			res = parseFloat(data[i].split('r')[1]);
-			if (res) > 1250000 {
+			if (res) < 1000 {
+				res = res.toFixed(2)
+			} else if (res) > 1250000 {
 				res = -1
-			} 
-			if (res) > 100 {
-				res = int(res)
+			} else {
+				res = res.toFixed(0)
 			}
 			
 			dataPoint[posRes] = res
