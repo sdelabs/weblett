d6ffd57208a1bbe93a3d51e8f1ea428284f942c3
SDE Labs account <152285936+sdelabs@users.noreply.github.com>
2024-08-29 11:16:19 +0200

    Update WebLETT.js

--- docs/WebLETT.js
+++ docs/WebLETT.js
@@ -727,7 +727,7 @@ window.addEventListener("beforeunload", function (e) {
 if (navigator.userAgent.includes("Firefox")) {
 	console.log("Firefox detected")
 	firefoxWarning.style.display = "initial";
-	firefoxWarning.style.backgroundColor = "#AACCCB";
+	firefoxWarning.style.color = "#FFCCCB";
 }
 
 console.log('WebLETT.js loaded');
