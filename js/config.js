3e0bfa80ca3a2ad94ce6a7e6d19422bfcff54770
SDE Labs account <152285936+sdelabs@users.noreply.github.com>
2026-04-22 15:23:38 +0200

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,6 @@
+/* 
+#### **A. `config.js` (De "Bron van Waarheid")**
+Hier zet je variabelen die overal nodig zijn, zoals je kleuren of standaardinstellingen.
+* *Functies:* Geen, alleen data.
+* *Inhoud:* `const CHART_COLORS = {...}`, `let debug = true;`.
+*/
