ff8bd8616effbd5a5a3be029bbd381c8410bd648
SDE Labs account <152285936+sdelabs@users.noreply.github.com>
2026-04-22 15:28:26 +0200

    Create ui-handlers.js

---
+++ docs/js/ui-handlers.js
@@ -0,0 +1,6 @@
+/*
+#### **D. `ui-handlers.js` (De Luisteraar)**
+Dit bestand koppelt de HTML aan de logica. Het reageert op de gebruiker.
+* *Functies:* `rbAxisClicked()`, `sensorClicked()`, `onSliderChange()`.
+* *Logica:* Hier lees je `.checked` of `.value` uit van de DOM en roep je functies aan in `chart-logic.js`.
+*/
