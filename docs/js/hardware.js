576343a7d9ac44978d1de4cccba39f51f2b2b0cd
SDE Labs account <152285936+sdelabs@users.noreply.github.com>
2026-04-22 15:26:27 +0200

    Create hardware.js

---
+++ docs/js/hardware.js
@@ -0,0 +1,6 @@
+/* 
+#### **C. `hardware.js` (De Prater)**
+Alles wat te maken heeft met de verbinding naar je LETT-apparaat.
+* *Functies:* `getReader()`, `startStop()`, `gripperUp()`, `listBLE()`.
+* *Logica:* WebUSB of WebSerial API calls.
+*/
