376002090cc414161d97b29c4ba0baa423b5daa7
SDE Labs account <152285936+sdelabs@users.noreply.github.com>
2026-04-22 15:25:48 +0200

    Create chart-logic.js

---
+++ docs/js/chart-logic.js
@@ -0,0 +1,6 @@
+/* 
+#### **B. `chart-logic.js` (De Tekenaar)**
+Dit bestand mag **alleen** weten hoe de grafiek werkt. Het mag niets weten over USB-poorten of knoppen.
+* *Functies:* `initChart()`, `updateChartData()`, `toggleDataset(index)`.
+* *Jouw code:* De `new Chart(...)` definitie hoort hier.
+*/
